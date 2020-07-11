349ca2ed0a388a40fcc0dccca64469d5978d251e
Nacho Davila <ignaciojoaquindavila@gmail.com>
2020-07-10 21:00:43 -0300

    commit inicial

---
+++ script.py
@@ -0,0 +1,24 @@
+####VARIABLES, se crea por ej: cualquier palabra o palabra con numero pero separando en el medio con "_"
+####VALUE, se le asigna un valor (value) a la variable elegida. ej: nombre_chico (VARIABLE) = "nacho" (VALUE)
+####PRINT, para reproducir el VALUE en consola utilizar "print (NOMBRE DE LA VARIABLE)".
+####STRING, SIEMPRE se separa entre ""(quotes en ingles) string es un valor.
+#### PARA IMPRIMIR UNA VARIABLE EN PRINT NO SE UTILIZAN COMILLAS, SOLO SE ENCIERRA EN ()
+#### PARA IMPRIMIR DIRECTAMENTE UN DATO EN CONSOLA SE DEBE UTILIZAR EL PRINT CON ("")
+
+
+
+name = "nacho" #La variable name almacena "nacho" 
+print (name) #indicamos la variable a imprimir, en consola se muestra el valor
+
+name_1 = "carlos"#La variable name_1 almacena "carlos"
+print (name_1) #indicamos la variable a imprimir, en consola se muestra el valor
+
+
+print ("Hello World!") #imprimie el dato directamente.
+
+
+
+objeto = "lapiz"
+print (objeto)
+print ("movie")
+print ("hola")
